0abfedd0e9cb12a397b41a62864c21c42bd45980
Olzhas Arystanov <olzhas.arystanov@reef.pl>
2024-05-04 01:48:36 +0500

    Add HyperParameter model, admin

--- app/src/bittensor_panel/core/admin.py
+++ app/src/bittensor_panel/core/admin.py
@@ -1,6 +1,19 @@
 from django.contrib import admin  # noqa
 from django.contrib.admin import register  # noqa
 
+from .models import HyperParameter
+
+
+@register(HyperParameter)
+class HyperParameterAdmin(admin.ModelAdmin):
+    list_display = ["name", "value", "created_at", "updated_at"]
+    
+    def has_add_permission(self, request):
+        return False
+
+    def has_delete_permission(self, request, obj=None):
+        return False
+
 
 admin.site.site_header = "Bittensor Administration Panel"
 admin.site.site_title = "Bittensor Administration Panel"
