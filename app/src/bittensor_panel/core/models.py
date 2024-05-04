cf9b3bb69a277883fb599af7a089d7af5b522339
Olzhas Arystanov <olzhas.arystanov@reef.pl>
2024-05-05 03:34:11 +0500

    Use BigIntegerField for HyperParameter values

--- app/src/bittensor_panel/core/models.py
+++ app/src/bittensor_panel/core/models.py
@@ -5,4 +5,4 @@ class HyperParameter(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     name = models.CharField(max_length=255, unique=True)
-    value = models.IntegerField()
+    value = models.BigIntegerField()
