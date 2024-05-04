990b01dc826345407800d3d26bb6067a65966f72
Olzhas Arystanov <olzhas.arystanov@reef.pl>
2024-05-05 02:45:53 +0500

    Use update_hyperparam service in admin

--- app/src/bittensor_panel/core/admin.py
+++ app/src/bittensor_panel/core/admin.py
@@ -1,18 +1,25 @@
-from django.contrib import admin  # noqa
-from django.contrib.admin import register  # noqa
+from django.contrib import admin, messages
+from django.contrib.admin import register
 
 from .models import HyperParameter
+from .services import HyperParameterUpdateFailed, update_hyperparam
 
 
 @register(HyperParameter)
 class HyperParameterAdmin(admin.ModelAdmin):
     list_display = ["name", "value", "created_at", "updated_at"]
-    
+
     def has_add_permission(self, request):
         return False
 
     def has_delete_permission(self, request, obj=None):
         return False
+
+    def save_model(self, request, obj, form, change):
+        try:
+            update_hyperparam(obj)
+        except HyperParameterUpdateFailed as e:
+            messages.error(request, str(e))
 
 
 admin.site.site_header = "Bittensor Administration Panel"
